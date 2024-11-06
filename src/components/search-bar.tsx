2cc06a075ab51d80db3b3851a6573034c807377b
Fede Marinas <fedemarinasruiz@gmail.com>
2024-11-06 14:13:53 +0100

    change favicon, metadata, search input style and handle enter key press

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -4,9 +4,21 @@ import { useState } from "react";
 export const SearchBar = ({ setSearchTerm }: { setSearchTerm: (searchTerm: string) => void }) => {
   const [term, setTerm] = useState("");
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setSearchTerm(term);
+    }
+  };
+
   return (
     <div className="rounded-full bg-white p-2 text-black border border-[#D8D8D8] flex items-center justify-between w-96">
-      <input placeholder="Search trips" type="text" onChange={(e) => setTerm(e.target.value)} />
+      <input
+        className="outline-none"
+        placeholder="Search trips"
+        type="text"
+        onChange={(e) => setTerm(e.target.value)}
+        onKeyDown={handleKeyPress}
+      />
       <button className="bg-black text-white rounded-full py-2 px-4" onClick={() => setSearchTerm(term)}>
         Search
       </button>
