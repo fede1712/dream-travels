b66c2ab81fda54f9f72c65a4c8cc3d670e251609
Fede Marinas <fedemarinasruiz@gmail.com>
2024-11-07 12:03:54 +0100

    solve error on add day

--- src/components/edit-trip-modal/edit-trip-modal.tsx
+++ src/components/edit-trip-modal/edit-trip-modal.tsx
@@ -27,6 +27,26 @@ export const EditTripModal = ({
     setIsEditTripModalOpen(false);
   };
 
+  const handleAddItineraryDay = () => {
+    setActualTrip((prevTrip) => {
+      const updatedItinerary: Itinerary[] = [
+        ...prevTrip.itinerary,
+        { day: prevTrip.itinerary.length + 1, location: "", description: "" },
+      ];
+      return { ...prevTrip, itinerary: updatedItinerary };
+    });
+
+    setItineraryDaysForm((prevElements) => [
+      ...prevElements,
+      <ItineraryForm
+        itineraryDaysFormLength={itineraryDaysForm.length}
+        key={itineraryDaysForm.length}
+        actualTrip={actualTrip}
+        setActualTrip={setActualTrip}
+      />,
+    ]);
+  };
+
   if (!isEditTripModalOpen) return null;
 
   return (
@@ -94,16 +114,7 @@ export const EditTripModal = ({
           </div>
           <label className="text-sm mb-2 flex justify-between items-center" htmlFor="name">
             <p>Day by day itinerary</p>
-            <button
-              role="add-button"
-              type="button"
-              onClick={() =>
-                setItineraryDaysForm((prevElements) => [
-                  ...prevElements,
-                  <ItineraryForm itineraryDaysFormLength={itineraryDaysForm.length} key={itineraryDaysForm.length} />,
-                ])
-              }
-            >
+            <button role="add-button" type="button" onClick={handleAddItineraryDay}>
               <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M10.5 19.25C15.3325 19.25 19.25 15.3325 19.25 10.5C19.25 5.66751 15.3325 1.75 10.5 1.75C5.66751 1.75 1.75 5.66751 1.75 10.5C1.75 15.3325 5.66751 19.25 10.5 19.25Z"
