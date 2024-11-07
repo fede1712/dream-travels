486d64014a1d99cc64e77e94dbe9950222d924a2
Fede Marinas <fedemarinasruiz@gmail.com>
2024-11-07 15:53:56 +0100

    solve loading on randon trip button

--- src/components/random-trip-button/random-trip-button.tsx
+++ src/components/random-trip-button/random-trip-button.tsx
@@ -6,12 +6,13 @@ import { RandomTripModal } from "../random-trip-modal/random-trip-modal";
 export const RandomTripButton = ({ filteredTrips }: { filteredTrips: Trip[] }) => {
   const [randomTrip, setRandomTrip] = useState<Trip | null>(null);
   const [isRandomTripModalOpen, setIsRandomTripModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getRandomTrip = () => {
     const randomIndex = Math.floor(Math.random() * filteredTrips.length);
     const selectedTrip = filteredTrips[randomIndex];
 
-    setRandomTrip(filteredTrips[randomIndex]);
+    setRandomTrip(selectedTrip);
     setIsRandomTripModalOpen(true);
 
     const targetDate = new Date();
@@ -22,18 +23,27 @@ export const RandomTripButton = ({ filteredTrips }: { filteredTrips: Trip[] }) =
   };
 
   useEffect(() => {
+    if (filteredTrips.length === 0) return;
+
     const storedTripData = localStorage.getItem("randomTrip");
+
     if (storedTripData) {
       const { id } = JSON.parse(storedTripData);
       const trip = filteredTrips.find((trip) => trip.id === id);
       if (trip) setRandomTrip(trip);
     }
+
+    setIsLoading(false);
   }, [filteredTrips]);
 
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-2 mb-10">
-        {!randomTrip ? (
+        {isLoading ? (
+          <div className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-slate-400 h-10 w-40"></div>
+          </div>
+        ) : !randomTrip ? (
           <>
             <h3>Don’t know where to go?</h3>
             <button
